4119bf8a1bfd54dd0f29bbf7375e808fbea2f20e
Juhani Valimaki <juhani.valimaki@haaga-helia.fi>
2025-03-10 10:23:33 +0200

    upd types express Request additions, just readability

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,6 +3,9 @@ import { RoleName, RoleRequired, User } from '../custom.js';
 declare global {
   namespace Express {
     export interface Request {
+      // This is the second part/extension of interface Request,
+      // the main part is imported from library 'express'.
+
       // For keeping the from token decrypted user obj in request
       user: User;
 
